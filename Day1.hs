613c1e5486aeb446da41440db3aeda461d57640e
Michael Hadley <mikethadley@gmail.com>
2019-11-30 23:19:49 -0800

    Initial commit and Day 1

---
+++ Day1.hs
@@ -0,0 +1,25 @@
+#!/usr/bin/env stack
+{- stack script
+    --resolver lts-14.16
+    --ghc-options -Wall
+-}
+import Data.Maybe (mapMaybe)
+import Text.Read (readMaybe)
+
+main :: IO ()
+main = do
+  let readInteger = readMaybe :: String -> Maybe Integer
+  masses <- mapMaybe readInteger <$> lines <$> readFile "Day1-input.txt"
+  let calculateWith f = show $ sum $ f <$> masses
+  putStrLn $ "Part 1: " <> calculateWith fuelRequired
+  putStrLn $ "Part 2: " <> calculateWith fuelRequiredPartTwo
+
+fuelRequired :: Integer -> Integer
+fuelRequired mass = (mass `div` 3) - 2
+
+fuelRequiredPartTwo :: Integer -> Integer
+fuelRequiredPartTwo mass =
+  let fuel = (mass `div` 3) - 2
+   in if fuel <= 0
+        then 0
+        else fuel + (fuelRequiredPartTwo fuel)
